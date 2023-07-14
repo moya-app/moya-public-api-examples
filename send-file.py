0fbc0989a8749380ce9a57154a42bbadd7a7e609
Mark Zealey <mark@markandruth.co.uk>
2023-07-14 11:17:31 +0100

    Optionally only upload image/asset without sendig

--- send-file.py
+++ send-file.py
@@ -3,13 +3,14 @@ from moya.messaging import API
 from moya.argtypes import number_or_file, setup_argparse
 
 parser = setup_argparse("Send file to moya users")
-parser.add_argument("numbers", type=number_or_file(), help="The number or file of numbers to send to")
 parser.add_argument("file", type=argparse.FileType('rb'), help="The file to send")
+parser.add_argument("numbers", nargs='?', type=number_or_file(), help="The number or file of numbers to send to")
 args = parser.parse_args()
 
 api = API(args.token, args.endpoint)
 
 file_url = api.upload_file(args.file)
 print(f"File uploaded to {file_url}")
-for numbers in args.numbers:
-    api.send_file(numbers, file_url, job_id=args.job_id, priority=args.priority)
+if args.numbers:
+    for numbers in args.numbers:
+        api.send_file(numbers, file_url, job_id=args.job_id, priority=args.priority)
--- send-image.py
+++ send-image.py
@@ -3,13 +3,14 @@ from moya.messaging import API
 from moya.argtypes import number_or_file, setup_argparse
 
 parser = setup_argparse("Send image to moya users")
-parser.add_argument("numbers", type=number_or_file(), help="The number or file of numbers to send to")
 parser.add_argument("image", type=argparse.FileType('rb'), help="The image to send")
+parser.add_argument("numbers", nargs='?', type=number_or_file(), help="The number or file of numbers to send to")
 args = parser.parse_args()
 
 api = API(args.token, args.endpoint)
 
 image_url = api.upload_image(args.image)
 print(f"Image uploaded to {image_url}")
-for numbers in args.numbers:
-    api.send_image(numbers, image_url, job_id=args.job_id, priority=args.priority)
+if args.numbers:
+    for numbers in args.numbers:
+        api.send_image(numbers, image_url, job_id=args.job_id, priority=args.priority)
